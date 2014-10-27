17ddfcc3f6c767c078e9aa2914d2a3d7b16a2fcc
zelfrontial <denisthamrim@Deniss-iMac.local>
2014-10-27 13:59:38 +1100

    Initial Commit

---
+++ StopnGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StopnGo
+//
+//  Created by Denis Thamrin on 27/10/2014.
+//  Copyright (c) 2014 ___DenisThamrin___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
